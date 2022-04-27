26275da3237753bebef38d2a220c38e0a925079b
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2022-04-27 19:42:45 +0200

    Add types for Duration and Size

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,15 @@
-export function durationToSeconds(duration: string): number {
+/** @pattern ^[0-9]+[smhdw]$ */
+export type Duration = string
+
+export function durationToSeconds(duration: Duration): number {
     const mapping = {'s': 1, 'm': 60, 'h': 3600, 'd': 86400, 'w': 604800}
     return parseInt(duration.substring(0, duration.length - 1), 10) * mapping[duration.substr(-1).toLowerCase() as 's' | 'm' | 'h' | 'd' | 'w']
 }
 
-export function sizeToKiB(size: string): number {
+/** @pattern ^[0-9]+[kmg]$ */
+export type Size = string
+
+export function sizeToKiB(size: Size): number {
     const mapping = {'k': 1, 'm': 1024, 'g': 1048576}
     return parseInt(size.substring(0, size.length - 1), 10)  * mapping[size.substr(-1).toLowerCase() as 'k' | 'm' | 'g']
 }
