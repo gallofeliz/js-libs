5f5590f80d41bac49dcb629db7ff51655f5195e7
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2023-04-04 15:18:32 +0200

    fix obfuscator tests

--- src/obfuscator/test.ts
+++ src/obfuscator/test.ts
@@ -2,37 +2,43 @@ import { deepEqual } from "assert"
 import {
     obfuscate,
     createObjectValuesByKeysObfuscatorProcessor,
-    createValuesObfuscatorProcessor
+    createValuesObfuscatorProcessor,
+    defaultProcessors
 } from "."
 
 describe('Obfuscator', () => {
-    it('test', () => {
 
-        const data = {
-            id: 54,
-            context: {
-                user: 'root',
-                password: 'root'
-            },
-            urls: {
-                main: 'https://user:pass@localhost'
-            },
-            email: 'toto@gmail.com',
-            firstName: 'Albert',
-            lastName: 'Dupont',
-            fullname: 'Albert Dupont',
-            age: 34,
-            sex: 'M',
-            very: {
-                deep: {
-                    object: {
-                        with: [
-                            [ '4444-3333-2222-1111', '192.168.0.1', 'ok' ]
-                        ]
-                    }
+    const data = {
+        id: 54,
+        context: {
+            user: 'root',
+            password: 'root'
+        },
+        urls: {
+            main: 'https://user:pass@localhost'
+        },
+        email: 'toto@gmail.com',
+        firstName: 'Albert',
+        lastName: 'Dupont',
+        fullname: 'Albert Dupont',
+        city: 'Madrid',
+        country: null,
+        zip: undefined,
+        age: 34,
+        sex: 'M',
+        very: {
+            deep: {
+                object: {
+                    city: 'Paris',
+                    with: [
+                        [ '4444-3333-2222-1111', '192.168.0.1', 'ok' ]
+                    ]
                 }
             }
         }
+    }
+
+    it('default processors', () => {
 
         deepEqual(
             obfuscate(data),
@@ -49,11 +55,15 @@ describe('Obfuscator', () => {
                 "firstName": "Albert",
                 "lastName": "Dupont",
                 "fullname": "Albert Dupont",
+                'city': 'Madrid',
+                'country': null,
+                'zip': undefined,
                 "age": 34,
                 "sex": "M",
                 "very": {
                     "deep": {
                         "object": {
+                            'city': 'Paris',
                             "with": [
                                 [
                                     "4444-3333-2222-1111",
@@ -67,37 +77,104 @@ describe('Obfuscator', () => {
             }
         )
 
+    })
+
+
+    it('extended default processors', () => {
+
+        deepEqual(
+            obfuscate(
+                data,
+                {
+                    ...defaultProcessors,
+                    emails: createObjectValuesByKeysObfuscatorProcessor('email')
+                }
+            ),
+            {
+                "id": 54,
+                "context": {
+                    "user": "root",
+                    "password": "***"
+                },
+                "urls": {
+                    "main": "https://user:***@localhost"
+                },
+                "email": "***",
+                "firstName": "Albert",
+                "lastName": "Dupont",
+                "fullname": "Albert Dupont",
+                'city': 'Madrid',
+                'country': null,
+                'zip': undefined,
+                "age": 34,
+                "sex": "M",
+                "very": {
+                    "deep": {
+                        "object": {
+                            'city': 'Paris',
+                            "with": [
+                                [
+                                    "4444-3333-2222-1111",
+                                    "192.168.0.1",
+                                    "ok"
+                                ]
+                            ]
+                        }
+                    }
+                }
+            }
+        )
+
+    })
+
+
+    it('custom processors', () => {
+
         deepEqual(
             obfuscate(
                 data,
                 [
-                    createObjectValuesByKeysObfuscatorProcessor(['email', /name/i, (v: string) => v === 'sex']),
-                    createValuesObfuscatorProcessor([/^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/, 'root', (v: string) => v === '192.168.0.1'])
+                    createObjectValuesByKeysObfuscatorProcessor('email'),
+                    createObjectValuesByKeysObfuscatorProcessor(/name/i),
+                    createObjectValuesByKeysObfuscatorProcessor((v: string) => v === 'sex'),
+                    createValuesObfuscatorProcessor(/^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/),
+                    createValuesObfuscatorProcessor('root'),
+                    createValuesObfuscatorProcessor((v: string) => v === '192.168.0.1'),
+                    (data, obstr) => {
+                        if (data instanceof Object && data.city === 'Paris') {
+                            data.city = obstr
+                        }
+                        return data
+                    }
                 ],
                 'SECRET'
             ),
             {
                 "id": 54,
                 "context": {
-                    "user": "root",
-                    "password": "root"
+                    "user": "SECRET",
+                    "password": "SECRET"
                 },
                 "urls": {
                     "main": "https://user:pass@localhost"
                 },
-                "email": "toto@gmail.com",
-                "firstName": "Albert",
-                "lastName": "Dupont",
-                "fullname": "Albert Dupont",
+                "email": "SECRET",
+                "firstName": "SECRET",
+                "lastName": "SECRET",
+                "fullname": "SECRET",
+                'city': 'Madrid',
+                'country': null,
+                'zip': undefined,
                 "age": 34,
-                "sex": "M",
+                "sex": "SECRET",
                 "very": {
                     "deep": {
                         "object": {
+                            'city': 'SECRET',
                             "with": [
                                 [
-                                    "4444-3333-2222-1111",
-                                    "192.168.0.1",
+                                    "SECRET",
+                                    "SECRET",
                                     "ok"
                                 ]
                             ]
