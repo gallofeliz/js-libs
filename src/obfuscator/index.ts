5abec2e318e0cce645291e16d94446fa03e454be
Gallo Feliz <40487446+gallofeliz@users.noreply.github.com>
2023-04-10 12:59:47 +0200

    obfuscator refacto again

--- src/obfuscator/index.ts
+++ src/obfuscator/index.ts
@@ -1,23 +1,58 @@
 import traverse from 'traverse'
 import { cloneDeep } from 'lodash'
 
-export type ObfuscatorRule<T = any> = (data: T, key: any, obfuscateString: string) => T | string
+export type ObfuscationRule<T = any> = (data: T, key: any, obfuscateString: string) => T | string
+
+// export interface ObfuscationRule2 {
+//     test: (value: any, key: string) => boolean
+//     obfuscate?: (value: any) => any
+// }
+
+export interface ObfuscatorOpts {
+    rules?: ObfuscationRule[]
+    replaceDefaultRules?: boolean
+    replacement?: string
+    softMode?: boolean
+}
+
+function createDefaultRules() {
+    return [
+        rulesBuilder.matchsUriCredentials(),
+        rulesBuilder.keyMatchsSecret()
+    ]
+}
 
 export class Obfuscator {
-    protected rules: ObfuscatorRule[]
+    protected rules: ObfuscationRule[]
     protected replacement: string
 
-    constructor(rules: ObfuscatorRule[], replacement: string = '***') {
-        this.rules = rules
-        this.replacement = replacement
+    constructor(opts: ObfuscatorOpts = {}) {
+        this.rules = (opts.rules || []).concat(opts.replaceDefaultRules ? [] : createDefaultRules())
+        this.replacement = opts.replacement || '***'
     }
-    obfuscate<T>(data: T): T extends Object ? T : T | string  {
+
+    public obfuscate<T>(data: T): T extends Object ? T : T | string  {
         if (this.rules.length === 0) {
             return data as any
         }
         const self = this
 
         return traverse(cloneDeep(data)).forEach(function (data: any) {
+            if (data instanceof Error) {
+                const newRawData = self.obfuscate({
+                    ...data,
+                    // name: data.name,
+                    message: data.message,
+                    stack: data.stack
+                })
+
+                const newData = Object.create(data)
+
+                Object.assign(newData, newRawData)
+
+                return newData
+            }
+
             for (const rule of self.rules) {
                 data = rule(data, this.key, self.replacement)
                 if (data === self.replacement) {
@@ -29,12 +64,12 @@ export class Obfuscator {
     }
 }
 
-export function obfuscate<T>(data: T, rules: ObfuscatorRule[], replacement?: string): T extends Object ? T : T | string {
-    return (new Obfuscator(rules, replacement)).obfuscate(data)
+export function obfuscate<T>(data: T, opts?: ObfuscatorOpts): T extends Object ? T : T | string {
+    return (new Obfuscator(opts)).obfuscate(data)
 }
 
-export const builtinRulesBuilders = {
-    keyMatchs(keyMatch: RegExp | ((data: any) => boolean) | any): ObfuscatorRule {
+export const rulesBuilder = {
+    keyMatchs(keyMatch: RegExp | ((data: any) => boolean) | any): ObfuscationRule {
         if (keyMatch instanceof RegExp) {
             return (data: any, key: any, obfuscateString: string) => {
                 return typeof key === 'string' && key.match(keyMatch) ? obfuscateString : data
@@ -51,7 +86,7 @@ export const builtinRulesBuilders = {
             return key === keyMatch ? obfuscateString : data
         }
     },
-    matchs(valueMatch: RegExp | ((data: any) => boolean) | any): ObfuscatorRule {
+    matchs(valueMatch: RegExp | ((data: any) => boolean) | any): ObfuscationRule {
         if (valueMatch instanceof RegExp) {
             return (data, _, obfuscateString: string) => {
                 return typeof data === 'string' && data.match(valueMatch) ? data.replace(valueMatch, obfuscateString) : data
@@ -68,11 +103,10 @@ export const builtinRulesBuilders = {
             return data === valueMatch ? obfuscateString : data
         }
     },
-    authInUrls: () => builtinRulesBuilders.matchs(/(?<=\/\/[^:]+:)[^@]+(?=@)/gi),
-    objKeysLooksLikeSecrets: () => builtinRulesBuilders.keyMatchs(/password|crendential|secret|token/i),
-    objKeysAreSecrets: () => builtinRulesBuilders.keyMatchs(/^auth$/i),
-    secretsInUrlsEncodedLike: () => builtinRulesBuilders.matchs(/(?<=(password|credentials|token)=)[^&; ]+/gi),
-    secretsInJsonLike: () => builtinRulesBuilders.matchs(/(?<="(password|credentials|token)": ?")(\\"|[^"])+(?=")/gi),
-    authInHeaders: () => builtinRulesBuilders.matchs(/(?<=authorization: ?\w+ )\w+/gi),
-    sessionsInCookies: () => builtinRulesBuilders.matchs(/(?<=Cookie:.*)(?<=(phpsessid|session)=)\w+/gi)
+    matchsUriCredentials: () => rulesBuilder.matchs(/(?<=\/\/[^:]+:)[^@]+(?=@)/gi),
+    keyMatchsSecret: () => rulesBuilder.keyMatchs(/^password|crendentials|secretKey/i),
+    secretsInUrlsEncodedLike: () => rulesBuilder.matchs(/(?<=(password|credentials|token)=)[^&; ]+/gi),
+    secretsInJsonLike: () => rulesBuilder.matchs(/(?<="(password|credentials|token)": ?")(\\"|[^"])+(?=")/gi),
+    authInHeaders: () => rulesBuilder.matchs(/(?<=authorization: ?\w+ )\w+/gi),
+    sessionsInCookies: () => rulesBuilder.matchs(/(?<=Cookie:.*)(?<=(phpsessid|session)=)\w+/gi)
 }
--- src/obfuscator/test.ts
+++ src/obfuscator/test.ts
@@ -1,257 +1,305 @@
-import { deepEqual, strictEqual } from 'assert'
+//import { deepEqual, strictEqual } from 'assert'
 import {
-    obfuscate,
-    builtinRulesBuilders
+    obfuscate
 } from '.'
+
+class MyCustomError extends Error {
+    name = 'MyError'
+    code = 'MY_ERROR'
+    protected data: any
+    constructor(message: string, data?: any) {
+        super(message)
+        this.data = data
+    }
+}
 
 describe('Obfuscator', () => {
 
-    const data = {
-        id: 54,
-        date: new Date,
-        error: new Error('Unable to connect to https://user:pass@localhost'),
-        context: {
-            user: 'root',
-            password: 'root'
-        },
-        urls: {
-            main: 'https://user:pass@localhost'
-        },
-        email: 'toto@gmail.com',
-        firstName: 'Albert',
-        lastName: 'Dupont',
-        fullname: 'Albert Dupont',
-        city: 'Madrid',
-        country: null,
-        zip: undefined,
-        age: 34,
-        sex: 'M',
-        very: {
-            deep: {
-                object: {
-                    city: 'Paris',
-                    with: [
-                        [ '4444-3333-2222-1111', '192.168.0.1', 'ok' ]
-                    ]
+    it('Simple test', () => {
+
+        const url = 'https://melanie:dontshouldseethat@mydomain/endpoint'
+
+        const data = {
+            message: 'Badaboom',
+            level: 'error',
+            config: {
+                url,
+                username: 'melanie',
+                password: 'dontshouldseethat',
+                server: {
+                    port: 80
                 }
-            }
+            },
+            thrownError: new MyCustomError('Invalid ' + url + ' : Unresolved DNS', { curl: { errno: 8, url } })
         }
-    }
 
-    it('simple value', () => {
-        strictEqual(
-            obfuscate('Hello https://melanie:secret@google.fr/path !', [builtinRulesBuilders.authInUrls()]),
-            'Hello https://melanie:***@google.fr/path !'
-        )
-    })
+        const obfuscated = obfuscate(data)
 
-    it('no rules', () => {
-        strictEqual(
-            obfuscate(data, []),
-            data
-        )
-    })
+        console.log(obfuscated)
 
-    it('all rules', () => {
+        console.log(data)
 
-        deepEqual(
-            obfuscate({
-                ...data,
-                form1: 'user=me&password=hacked',
-                form2: 'credentials=hacked%26!&user=me',
-                stringifiedJson1: JSON.stringify({user: 'me', password: 'hacked'}),
-                stringifiedJson2: JSON.stringify({password: 'hacked"!', user: 'me'}),
-                headers: 'Content-Type: text/plain'
-                + '\nAuthorization: Basic YWxhZGRpbjpvcGVuc2VzYW1l'
-                + '\nCookie: PHPSESSID=298zf09hf012fh2; csrftoken=u32t4o3tb3gg43; _gat=1'
-                + '\nCache-Control: max-age=604800'
-            }, [
-                builtinRulesBuilders.authInHeaders(),
-                builtinRulesBuilders.authInUrls(),
-                builtinRulesBuilders.objKeysAreSecrets(),
-                builtinRulesBuilders.objKeysLooksLikeSecrets(),
-                builtinRulesBuilders.secretsInJsonLike(),
-                builtinRulesBuilders.secretsInUrlsEncodedLike(),
-                builtinRulesBuilders.sessionsInCookies()
-            ]),
-            {
-                'id': 54,
-                'date': data.date,
-                'error': data.error,
-                'context': {
-                    'user': 'root',
-                    'password': '***'
-                },
-                'urls': {
-                    'main': 'https://user:***@localhost'
-                },
-                'email': 'toto@gmail.com',
-                'firstName': 'Albert',
-                'lastName': 'Dupont',
-                'fullname': 'Albert Dupont',
-                'city': 'Madrid',
-                'country': null,
-                'zip': undefined,
-                'age': 34,
-                'sex': 'M',
-                'form1': 'user=me&password=***',
-                'form2': 'credentials=***&user=me',
-                'stringifiedJson1': JSON.stringify({user: 'me', password: '***'}),
-                'stringifiedJson2': JSON.stringify({password: '***', user: 'me'}),
-                'headers': 'Content-Type: text/plain'
-                + '\nAuthorization: Basic ***'
-                + '\nCookie: PHPSESSID=***; csrftoken=***; _gat=1'
-                + '\nCache-Control: max-age=604800',
-                'very': {
-                    'deep': {
-                        'object': {
-                            'city': 'Paris',
-                            'with': [
-                                [
-                                    '4444-3333-2222-1111',
-                                    '192.168.0.1',
-                                    'ok'
-                                ]
-                            ]
-                        }
-                    }
-                }
-            }
-        )
 
     })
 
-    it('extended all rules', () => {
 
-        deepEqual(
-            obfuscate(
-                data,
-                [
-                    builtinRulesBuilders.authInHeaders(),
-                    builtinRulesBuilders.authInUrls(),
-                    builtinRulesBuilders.objKeysAreSecrets(),
-                    builtinRulesBuilders.objKeysLooksLikeSecrets(),
-                    builtinRulesBuilders.secretsInJsonLike(),
-                    builtinRulesBuilders.secretsInUrlsEncodedLike(),
-                    builtinRulesBuilders.sessionsInCookies(),
-                    builtinRulesBuilders.keyMatchs('email')
-                ]
-            ),
-            {
-                'id': 54,
-                'date': data.date,
-                'error': data.error,
-                'context': {
-                    'user': 'root',
-                    'password': '***'
-                },
-                'urls': {
-                    'main': 'https://user:***@localhost'
-                },
-                'email': '***',
-                'firstName': 'Albert',
-                'lastName': 'Dupont',
-                'fullname': 'Albert Dupont',
-                'city': 'Madrid',
-                'country': null,
-                'zip': undefined,
-                'age': 34,
-                'sex': 'M',
-                'very': {
-                    'deep': {
-                        'object': {
-                            'city': 'Paris',
-                            'with': [
-                                [
-                                    '4444-3333-2222-1111',
-                                    '192.168.0.1',
-                                    'ok'
-                                ]
-                            ]
-                        }
-                    }
-                }
-            }
-        )
 
-    })
 
-    it('custom rules', () => {
 
-        let expectedErr;
+    // const data = {
+    //     id: 54,
+    //     33: 34,
+    //     [Symbol.for('test')]: true,
+    //     date: new Date,
+    //     error: new Error('Unable to connect to https://user:pass@localhost'),
+    //     context: {
+    //         user: 'root',
+    //         password: 'root'
+    //     },
+    //     urls: {
+    //         main: 'https://user:pass@localhost'
+    //     },
+    //     email: 'toto@gmail.com',
+    //     firstName: 'Albert',
+    //     lastName: 'Dupont',
+    //     fullname: 'Albert Dupont',
+    //     city: 'Madrid',
+    //     country: null,
+    //     zip: undefined,
+    //     age: 34,
+    //     sex: 'M',
+    //     very: {
+    //         deep: {
+    //             object: {
+    //                 city: 'Paris',
+    //                 with: [
+    //                     [ '4444-3333-2222-1111', '192.168.0.1', 'ok' ]
+    //                 ]
+    //             }
+    //         }
+    //     }
+    // }
 
-        deepEqual(
-            obfuscate(
-                data,
-                [
-                    builtinRulesBuilders.keyMatchs('email'),
-                    builtinRulesBuilders.keyMatchs(/name/i),
-                    builtinRulesBuilders.keyMatchs((v: string) => v === 'sex'),
-                    builtinRulesBuilders.matchs(/^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/),
-                    builtinRulesBuilders.matchs('root'),
-                    builtinRulesBuilders.matchs((v: string) => v === '192.168.0.1'),
-                    (data, key, obstr) => {
-                        if (key === 'city' && data === 'Paris') {
-                            return obstr
-                        }
-                        return data
-                    },
-                    (data, key, obstr) => {
-                        if (data instanceof Error) {
-                            const newMsg = data.message.replace(/(?<=\/\/[^:]+:)[^@]+(?=@)/gi, obstr)
+    // it('simple value', () => {
+    //     strictEqual(
+    //         obfuscate('Hello https://melanie:secret@google.fr/path !', [builtinRulesBuilders.authInUrls()]),
+    //         'Hello https://melanie:***@google.fr/path !'
+    //     )
+    // })
 
-                            if (data.message !== newMsg) {
-                                const newError = new Error(newMsg)
+    // it('no rules', () => {
+    //     strictEqual(
+    //         obfuscate(data, []),
+    //         data
+    //     )
+    // })
 
-                                // etc
+    // it('all rules', () => {
 
-                                expectedErr = newError
+    //     deepEqual(
+    //         obfuscate({
+    //             ...data,
+    //             form1: 'user=me&password=hacked',
+    //             form2: 'credentials=hacked%26!&user=me',
+    //             stringifiedJson1: JSON.stringify({user: 'me', password: 'hacked'}),
+    //             stringifiedJson2: JSON.stringify({password: 'hacked"!', user: 'me'}),
+    //             headers: 'Content-Type: text/plain'
+    //             + '\nAuthorization: Basic YWxhZGRpbjpvcGVuc2VzYW1l'
+    //             + '\nCookie: PHPSESSID=298zf09hf012fh2; csrftoken=u32t4o3tb3gg43; _gat=1'
+    //             + '\nCache-Control: max-age=604800'
+    //         }, [
+    //             builtinRulesBuilders.authInHeaders(),
+    //             builtinRulesBuilders.authInUrls(),
+    //             builtinRulesBuilders.objKeysAreSecrets(),
+    //             builtinRulesBuilders.objKeysLooksLikeSecrets(),
+    //             builtinRulesBuilders.secretsInJsonLike(),
+    //             builtinRulesBuilders.secretsInUrlsEncodedLike(),
+    //             builtinRulesBuilders.sessionsInCookies()
+    //         ]),
+    //         {
+    //             'id': 54,
+    //             33: 34,
+    //             [Symbol.for('test')]: true,
+    //             'date': data.date,
+    //             'error': data.error,
+    //             'context': {
+    //                 'user': 'root',
+    //                 'password': '***'
+    //             },
+    //             'urls': {
+    //                 'main': 'https://user:***@localhost'
+    //             },
+    //             'email': 'toto@gmail.com',
+    //             'firstName': 'Albert',
+    //             'lastName': 'Dupont',
+    //             'fullname': 'Albert Dupont',
+    //             'city': 'Madrid',
+    //             'country': null,
+    //             'zip': undefined,
+    //             'age': 34,
+    //             'sex': 'M',
+    //             'form1': 'user=me&password=***',
+    //             'form2': 'credentials=***&user=me',
+    //             'stringifiedJson1': JSON.stringify({user: 'me', password: '***'}),
+    //             'stringifiedJson2': JSON.stringify({password: '***', user: 'me'}),
+    //             'headers': 'Content-Type: text/plain'
+    //             + '\nAuthorization: Basic ***'
+    //             + '\nCookie: PHPSESSID=***; csrftoken=***; _gat=1'
+    //             + '\nCache-Control: max-age=604800',
+    //             'very': {
+    //                 'deep': {
+    //                     'object': {
+    //                         'city': 'Paris',
+    //                         'with': [
+    //                             [
+    //                                 '4444-3333-2222-1111',
+    //                                 '192.168.0.1',
+    //                                 'ok'
+    //                             ]
+    //                         ]
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     )
 
-                                return newError
-                            }
-                        }
+    // })
 
-                        return data
-                    }
-                ],
-                'SECRET'
-            ),
-            {
-                'id': 54,
-                'date': data.date,
-                'error': expectedErr,
-                'context': {
-                    'user': 'SECRET',
-                    'password': 'SECRET'
-                },
-                'urls': {
-                    'main': 'https://user:pass@localhost'
-                },
-                'email': 'SECRET',
-                'firstName': 'SECRET',
-                'lastName': 'SECRET',
-                'fullname': 'SECRET',
-                'city': 'Madrid',
-                'country': null,
-                'zip': undefined,
-                'age': 34,
-                'sex': 'SECRET',
-                'very': {
-                    'deep': {
-                        'object': {
-                            'city': 'SECRET',
-                            'with': [
-                                [
-                                    'SECRET',
-                                    'SECRET',
-                                    'ok'
-                                ]
-                            ]
-                        }
-                    }
-                }
-            }
-        )
+    // it('extended all rules', () => {
 
-    })
+    //     deepEqual(
+    //         obfuscate(
+    //             data,
+    //             [
+    //                 builtinRulesBuilders.authInHeaders(),
+    //                 builtinRulesBuilders.authInUrls(),
+    //                 builtinRulesBuilders.objKeysAreSecrets(),
+    //                 builtinRulesBuilders.objKeysLooksLikeSecrets(),
+    //                 builtinRulesBuilders.secretsInJsonLike(),
+    //                 builtinRulesBuilders.secretsInUrlsEncodedLike(),
+    //                 builtinRulesBuilders.sessionsInCookies(),
+    //                 builtinRulesBuilders.keyMatchs('email')
+    //             ]
+    //         ),
+    //         {
+    //             'id': 54,
+    //             33: 34,
+    //             [Symbol.for('test')]: true,
+    //             'date': data.date,
+    //             'error': data.error,
+    //             'context': {
+    //                 'user': 'root',
+    //                 'password': '***'
+    //             },
+    //             'urls': {
+    //                 'main': 'https://user:***@localhost'
+    //             },
+    //             'email': '***',
+    //             'firstName': 'Albert',
+    //             'lastName': 'Dupont',
+    //             'fullname': 'Albert Dupont',
+    //             'city': 'Madrid',
+    //             'country': null,
+    //             'zip': undefined,
+    //             'age': 34,
+    //             'sex': 'M',
+    //             'very': {
+    //                 'deep': {
+    //                     'object': {
+    //                         'city': 'Paris',
+    //                         'with': [
+    //                             [
+    //                                 '4444-3333-2222-1111',
+    //                                 '192.168.0.1',
+    //                                 'ok'
+    //                             ]
+    //                         ]
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     )
+
+    // })
+
+    // it('custom rules', () => {
+
+    //     let expectedErr;
+
+    //     deepEqual(
+    //         obfuscate(
+    //             data,
+    //             [
+    //                 builtinRulesBuilders.keyMatchs('email'),
+    //                 builtinRulesBuilders.keyMatchs(/name/i),
+    //                 builtinRulesBuilders.keyMatchs((v: string) => v === 'sex'),
+    //                 builtinRulesBuilders.matchs(/^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/),
+    //                 builtinRulesBuilders.matchs('root'),
+    //                 builtinRulesBuilders.matchs((v: string) => v === '192.168.0.1'),
+    //                 (data, key, obstr) => {
+    //                     if (key === 'city' && data === 'Paris') {
+    //                         return obstr
+    //                     }
+    //                     return data
+    //                 },
+    //                 (data, key, obstr) => {
+    //                     if (data instanceof Error) {
+    //                         const newMsg = data.message.replace(/(?<=\/\/[^:]+:)[^@]+(?=@)/gi, obstr)
+
+    //                         if (data.message !== newMsg) {
+    //                             const newError = new Error(newMsg)
+
+    //                             // etc
+
+    //                             expectedErr = newError
+
+    //                             return newError
+    //                         }
+    //                     }
+
+    //                     return data
+    //                 }
+    //             ],
+    //             'SECRET'
+    //         ),
+    //         {
+    //             'id': 54,
+    //             33: 34,
+    //             [Symbol.for('test')]: true,
+    //             'date': data.date,
+    //             'error': expectedErr,
+    //             'context': {
+    //                 'user': 'SECRET',
+    //                 'password': 'SECRET'
+    //             },
+    //             'urls': {
+    //                 'main': 'https://user:pass@localhost'
+    //             },
+    //             'email': 'SECRET',
+    //             'firstName': 'SECRET',
+    //             'lastName': 'SECRET',
+    //             'fullname': 'SECRET',
+    //             'city': 'Madrid',
+    //             'country': null,
+    //             'zip': undefined,
+    //             'age': 34,
+    //             'sex': 'SECRET',
+    //             'very': {
+    //                 'deep': {
+    //                     'object': {
+    //                         'city': 'SECRET',
+    //                         'with': [
+    //                             [
+    //                                 'SECRET',
+    //                                 'SECRET',
+    //                                 'ok'
+    //                             ]
+    //                         ]
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     )
+
+    //})
 })
